c6e26367c53e32cf7d592f0745b83aa9fc99393b
elizavetavalyalkina6 <g5hbh57784@privaterelay.appleid.com>
2026-02-04 00:03:21 +0300

    - check linter

---
+++ main.py
@@ -0,0 +1,21 @@
+class ExampleClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calculate(self):
+        result = self.a + self.b
+        print(result)
+
+
+def main():
+    x = 5
+    y = 10
+    z = x + y
+    print("The result is ", z)
+
+
+if __name__ == "__main__":
+    main()
+if False:
+    pass
